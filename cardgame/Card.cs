8f86c41d31926e983fe01a25da2484a79de9becb
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-11-23 13:16:34 +0100

    first commit

---
+++ cardgame/Card.cs
@@ -0,0 +1,5 @@
+public class Card
+{
+    public int value { get; set; }
+    public string type { get; set; }
+}
---
+++ cardgame/Program.cs
@@ -0,0 +1,11 @@
+﻿intro();
+
+void intro() {
+    Console.WriteLine("What cardgame would you like to play?");
+    Console.WriteLine("Current choices\n___\nBlackjack");
+    string choice = Console.ReadLine();
+}
+
+
+
+Console.ReadLine();
