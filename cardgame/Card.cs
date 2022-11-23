ed2958d527c369761f256ca6ec43a183006f9a65
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-11-23 13:33:01 +0100

    start

--- cardgame/Card.cs
+++ cardgame/Card.cs
@@ -1,5 +1,6 @@
 public class Card
 {
+    public Random rnd = new Random();
     public int value { get; set; }
     public string type { get; set; }
 }
--- cardgame/Program.cs
+++ cardgame/Program.cs
@@ -1,9 +1,28 @@
-﻿intro();
+﻿Card card = new Card();
+Random rnd = new Random();
+card.value = rnd.Next(2, 10);
+
+intro();
 
 void intro() {
+    Console.Clear();
     Console.WriteLine("What cardgame would you like to play?");
-    Console.WriteLine("Current choices\n___\nBlackjack");
-    string choice = Console.ReadLine();
+    Console.WriteLine("Current choices\n<------|------>\nBlackjack\n\nPlease choose what you would like to play:");
+    string choice = Console.ReadLine().ToLower();
+    if (choice == "blackjack") {
+        blackjack();
+    }
+    else {
+        Console.WriteLine("That is not a valid choice");
+        intro();
+    }
+}
+
+void blackjack() {
+    Console.Clear();
+    Console.WriteLine("Welcome to Blackjack!");
+    Console.WriteLine(card.type);
+    Console.WriteLine(card.value);
 }
 
 
