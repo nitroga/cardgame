b27c0740ed26927d7a59a25af2695ef738b98604
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-11-25 13:14:59 +0100

    cards in a list

--- cardgame/Program.cs
+++ cardgame/Program.cs
@@ -1,30 +1,24 @@
-﻿Card c = new Card();
-Card cTwo = new Card();
-Card dealerC = new Card();
-Card dealerCtwo = new Card();
-Random rnd = new Random();
-for (int i = 0; i < 3; i++)
+﻿Random rnd = new Random();
+List<Card> cards = new List<Card>() {new Card(), new Card()};
+List<Card> dealerCards = new List<Card>() {new Card(), new Card()};
+
+for (int i = 0; i < cards.Count; i++)
 {
-    
+    cards[i].type = cards[i].types[rnd.Next(4)];
+    cards[i].value = rnd.Next(2, 10);
+    dealerCards[i].type = dealerCards[i].types[rnd.Next(4)];
+    dealerCards[i].value = rnd.Next(2, 10);
 }
-c.value = rnd.Next(2, 10);
-c.type = c.types[rnd.Next(4)];
-cTwo.value = rnd.Next(2, 10);
-cTwo.type = c.types[rnd.Next(4)];
-dealerC.value = rnd.Next(2, 10);
-dealerC.type = c.types[rnd.Next(4)];
-dealerCtwo.value = rnd.Next(2, 10);
-dealerCtwo.type = c.types[rnd.Next(4)];
 
 intro();
 
 void intro() {
-    Console.Clear();
+    //Console.Clear();
     Console.WriteLine("What cardgame would you like to play?");
     Console.WriteLine("Current choices\n<------|------>\nBlackjack\n\nPlease choose what you would like to play:");
     string choice = Console.ReadLine().ToLower();
     if (choice == "blackjack") {
-        blackjack();
+        dealingBlackjack();
     }
     else {
         Console.WriteLine("That is not a valid choice");
@@ -32,17 +26,47 @@ void intro() {
     }
 }
 
-void blackjack() {
+void dealingBlackjack() {
     Console.Clear();
     Console.WriteLine("Welcome to Blackjack!\n");
     //Console.WriteLine("Please place your bets");
     Console.WriteLine("Generating cards...\nYour cards are:");
-    Console.WriteLine(c.type + " " + c.value);
-    Console.WriteLine(cTwo.type + " " + cTwo.value);
+    Console.WriteLine(cards[0].type + " " + cards[0].value);
+    Console.WriteLine(cards[1].type + " " + cards[1].value);
+    Console.WriteLine($"You have {cards[0].value + cards[1].value}");
     Console.WriteLine("\nThe dealer cards are:");
-    Console.WriteLine(dealerC.type + " " + dealerCtwo.value);
+    Console.WriteLine(dealerCards[0].type + " " + dealerCards[0].value);
     Console.WriteLine("Hidden  Hidden");
+    Console.WriteLine($"The dealer has {dealerCards[0].value}");
+    if (cards[0].value + cards[1].value == 21) {
+        Console.WriteLine("You got a blackjack!");
+    }
+    if (dealerCards[0].value + dealerCards[1].value == 21) {
+        Console.WriteLine("The dealer got a blackjack!");
+    }
+    blackjack();
+}
+
+void blackjack() {
+    Console.WriteLine("\nWhat would you like to do?\nHit\nStand\nDouble Down");
+    string choice = Console.ReadLine().ToLower();
+    if (choice == "hit") {
+        blackjackHit();
+    }
+
+    if (choice == "stand") {
+        blackjackStand();
+    }
+}
+
+void blackjackHit () {
+    Console.Clear();
 
 }
+
+void blackjackStand () {
+    Console.Clear();
+}
+
 
 Console.ReadLine();
